63004dded5ea174090b04d8fa7bbf09f2dfcec3f
IBruteDude <kamarf505@gmail.com>
2024-06-07 05:02:04 +0300

    Did the lockboxes project

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module for the connected graph algorithm
+"""
+from queue import Queue
+
+
+def canUnlockAll(boxes):
+	""" Check if an adjacency list represented graph is connected
+	"""
+	if not boxes:
+		return True
+
+	slots = [False] * len(boxes)
+	q = Queue(len(boxes))
+
+	slots[0] = True
+	for key in boxes[0]:
+		slots[key] = True
+		q.put(key)
+
+	while not q.empty():
+		for key in boxes[q.get()]:
+			if not slots[key]:
+				slots[key] = True
+				q.put(key)
+	
+	return all(slots)
