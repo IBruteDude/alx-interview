f79779b647186ba9af1bc389d84c769ce353eb98
IBruteDude <kamarf505@gmail.com>
2024-06-07 05:04:58 +0300

    Fixed out-of-bounds bug and code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,23 +5,25 @@ from queue import Queue
 
 
 def canUnlockAll(boxes):
-	""" Check if an adjacency list represented graph is connected
-	"""
-	if not boxes:
-		return True
+    """ Check if an adjacency list represented graph is connected
+    """
+    if not boxes:
+        return True
 
-	slots = [False] * len(boxes)
-	q = Queue(len(boxes))
+    slots = [False] * len(boxes)
+    q = Queue(len(boxes))
 
-	slots[0] = True
-	for key in boxes[0]:
-		slots[key] = True
-		q.put(key)
+    slots[0] = True
+    for key in boxes[0]:
+        slots[key] = True
+        q.put(key)
 
-	while not q.empty():
-		for key in boxes[q.get()]:
-			if not slots[key]:
-				slots[key] = True
-				q.put(key)
-	
-	return all(slots)
+    while not q.empty():
+        opening_key = q.get()
+        if opening_key < len(boxes):
+            for key in boxes[opening_key]:
+                if not slots[key]:
+                    slots[key] = True
+                    q.put(key)
+
+    return all(slots)
