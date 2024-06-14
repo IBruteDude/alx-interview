6622910b8873d051ec95549eccb7a32b9cb19a65
IBruteDude <kamarf505@gmail.com>
2024-06-14 05:14:37 +0300

    Did the  min factoring ops task, AlHamdulillah

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module for minimum prime factoring operation of a number
+"""
+
+
+def minOperations(n: int) -> int:
+    """ Calculate minimum number of multiplications to reach 'n'
+    """
+    op_count = 0
+    if n <= 1:
+        return 0
+
+    for i in range(2, n + 1):
+        if n % i == 0:
+            op_count += i
+            n //= i
+            while n % i == 0:
+                op_count += i
+                n //= i
+    return op_count
+
+
+if __name__ == '__main__':
+    n = 13
+    print(f"Min # of operations to reach {n} char: {minOperations(n)}")
+
+    n = 12
+    print(f"Min # of operations to reach {n} char: {minOperations(n)}")
