c6a4f0c63e5b69cb43f5064a83a7f4851b49a0cb
IBruteDude <kamarf505@gmail.com>
2024-06-20 23:17:20 +0300

    Did the file stat logger

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Module for defining the log stats parser
+"""
+import sys
+import re
+
+
+itercount = 0
+file_size = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+
+def print_stats():
+    """ print the current stats
+    """
+    print(f"File size: {file_size}", flush=True)
+    for code, count in sorted(status_codes.items(), key=lambda s: int(s[0])):
+        if count:
+            print(f"{code}: {count}", flush=True)
+
+
+try:
+    for line in sys.stdin:
+        itercount += 1
+        if line is not None:
+            m = re.match(r"""(\d*.\d*.\d*.\d*) - \[(.*)\] \"GET /projects/260 HTTP/1.1\" (\d*) (\d*)""", line)
+            ipaddr, date, status_code, size = m.groups()
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            file_size += int(size)
+            if itercount == 10:
+                print_stats()
+                itercount = 0
+except KeyboardInterrupt:
+    print_stats()
