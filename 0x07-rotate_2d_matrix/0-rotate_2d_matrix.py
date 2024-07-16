e87ae6030915948933032143feb1fba4fc6c3f83
IBruteDude <kamarf505@gmail.com>
2024-07-16 18:10:49 +0300

    It was the shebang

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Module for a matrix rotating function
 """
 
@@ -7,14 +7,21 @@ def rotate_2d_matrix(matrix):
     """ In-place matrix rotating function
     """
     # the more pythonic one-liner
-    newmatrix = list(map(list, zip(*reversed(matrix))))
-    for i in range(len(newmatrix)):
-        matrix[i] = newmatrix[i]
+    # newmatrix = list(map(list, zip(*reversed(matrix))))
+
+    n = len(matrix)
+    for lvl in range(n//2):
+        for slot in range(lvl, n-lvl - 1):
+            tmp = matrix[lvl][slot]
+            matrix[lvl][slot] = matrix[n-slot-1][lvl]
+            matrix[n-slot-1][lvl] = matrix[n-lvl-1][n-slot-1]
+            matrix[n-lvl-1][n-slot-1] = matrix[slot][n-lvl-1]
+            matrix[slot][n-lvl-1] = tmp
 
 
 if __name__ == "__main__":
-    matrix = [[1,  2,  3,  4],
-              [5,  6,  7,  8],
+    matrix = [[1,   2,   3, 4],
+              [5,   6,  7, 8],
               [9,  10, 11, 12],
               [13, 14, 15, 16]]
 
