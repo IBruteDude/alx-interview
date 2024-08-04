94eee09645ef8757c1202693e5cc710ca7c196b0
IBruteDude <kamarf505@gmail.com>
2024-08-04 22:30:10 +0300

    Removed random empty lines

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -30,16 +30,13 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: nqueens N")
         exit(1)
-
     try:
         N = int(sys.argv[1])
     except Exception:
         print("N must be a number")
         exit(1)
-
     if N < 4:
         print("N must be at least 4")
         exit(1)
-
     for solution in n_queens_solutions(N):
         print(solution)
