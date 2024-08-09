1de3216b034c9bf0e86d4d2c160e24104f0c1d7b
IBruteDude <kamarf505@gmail.com>
2024-08-09 04:25:57 +0300

    Did some cleanup

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const { argv } = require('process');
-let chrs = [];
 const mp = new Map();
-request(`https://swapi-api.alx-tools.com/api/films/${argv[2]}/?format=json`, (error, response, body) => {
+let chrs = [];
+const filmApiUrl = `https://swapi-api.alx-tools.com/api/films/${argv[2]}/?format=json`;
+
+request(filmApiUrl, (error, _, body) => {
   if (error) throw error;
   chrs = JSON.parse(body).characters;
   for (const character of chrs) {
-    request(character.concat('?format=json'), (error, response, body) => {
+    request(character.concat('?format=json'), (error, _, body) => {
       if (error) throw error;
       mp.set(character, JSON.parse(body).name);
       if (mp.size === chrs.length) for (const chr of chrs) console.log(mp.get(chr));
